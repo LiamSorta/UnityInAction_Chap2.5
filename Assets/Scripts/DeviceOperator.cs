64fcf6c70801094f0367d5fe183437040f5832de
Anthony Fadale <anthonyfadale@gmail.com>
2015-12-28 04:12:59 -0500

    Added DeviceOperator and DoorOpenDevice, Added Door and Column objects to Scene, Saved Scene.

---
+++ Assets/Scripts/DeviceOperator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviceOperator : MonoBehaviour {
+	public float radius = 1.5f;																// How far away from the player to activate devices
+
+	void Update() {
+		if (Input.GetButtonDown ("Fire3")) {												// Respond to the input button defined in Unity's input settings.
+			Collider[] hitColliders = Physics.OverlapSphere (transform.position, radius);	// OverlapSphere() returns a list of nearby objects
+			foreach (Collider hitCollider in hitColliders) {								
+				hitCollider.SendMessage ("Operate", SendMessageOptions.DontRequireReceiver);// SendMessage tries to call the named function, regardless of the target's type.
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/DoorOpenDevice.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpenDevice : MonoBehaviour {
+	[SerializeField] private Vector3 dPos;			// The position to offset to when the door opens
+
+	private bool _open;								// A Boolean to keep track of the open state of the door
+
+	public void Operate(){
+		if (_open) {								// Open or close the door depending on the open state.
+			Vector3 pos = transform.position - dPos;
+			transform.position = pos;
+		} else {
+			Vector3 pos = transform.position + dPos;
+			transform.position = pos;
+		}
+		_open = !_open;
+	}
+}
