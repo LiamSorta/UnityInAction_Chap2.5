82edc5a8ec97476613540cdf8b0a310703558bbd
Anthony Fadale <anthonyfadale@gmail.com>
2015-12-28 06:05:59 -0500

    Added Materials folder, Materials for specific collectible items, Added CollectibleItems script, Added items to sceen, saved scene

---
+++ Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleItem : MonoBehaviour {
+	[SerializeField] private string itemName;		// type the name of this item in the Inspector.
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Item collected: " + itemName);
+		Destroy (this.gameObject);
+	}
+}
